0661384ef42f8b9e0b9e5d3adb0b5cc32a0d6d93
Mark Travis <mtravis@ripple.com>
2015-01-31 08:15:52 -0800

    Synchronize worker start time, ramp up transactions.

--- benchmark/conf.py
+++ benchmark/conf.py
@@ -8,3 +8,5 @@ summary_interval = 10
 rate = 2000
 # thread stack size in KB
 stack_size = 128
+# ramp up time in seconds
+ramp = 60
--- benchmark/harness.py
+++ benchmark/harness.py
@@ -29,13 +29,21 @@ import random
 # 3. synchronize time
 # 4. start
 
-PAUSE_BEFORE_STARTING = 5
-SCHEDULE_BACKOFF = 5
+SCHEDULE_BACKOFF = 1
 
-def worker(test, q, instance, scheduler_state, rate, under_threshold,
+def worker(test, q, instance, workers, scheduler_state, rate, under_threshold,
            over_threshold):
+    scheduler_state._waiting_lock.acquire()
+    scheduler_state._waiting += 1
+    scheduler_state._waiting_lock.release()
+    # Faster than condition notify(a zillion).
+    while scheduler_state._waiting < workers:
+        time.sleep(1)
+    if scheduler_state._start_second == 0:
+        scheduler_state._start_second = int(time.time())
+
     while True:
-        pause(scheduler_state, rate)
+        scheduler_state.pause()
 
         scheduler_state._started_lock.acquire()
         scheduler_state._started += 1
@@ -61,36 +69,49 @@ def worker(test, q, instance, scheduler_state, rate, under_threshold,
                results[1:]])
 
 
-def pause(scheduler_state, rate):
-    scheduler_state._bucket_lock.acquire()
-    scheduler_state._bucket_entries += 1
-    if (scheduler_state._bucket_entries >= rate):
-        scheduler_state._bucket_second += 1
-        scheduler_state._bucket_entries = 0
-    scheduler_state._bucket_lock.release()
-
-    # Have we fallen behind?
-    now = time.time()
-    if scheduler_state._bucket_second <= now:
-        scheduler_state._bucket_second = now + SCHEDULE_BACKOFF
-    time.sleep(scheduler_state._bucket_second - now +\
-                   ((float(random.randint(0, 1000)))/1000 ))
-
-
 class SchedulerState:
     _started = 0
     _finished = 0
     _duration = 0
     _under_threshold = 0
     _over_threshold = 0
-    _bucket_second = PAUSE_BEFORE_STARTING
+    _bucket_second = 0
     _bucket_entries = 0
+    _waiting = 0
+    _start_second = 0
 
-    def __init__(self, statuses=1):
+    def __init__(self, rate, ramp, statuses=1):
+        self._rate = rate
+        self._ramp = ramp
         self._status = [0] * statuses
         self._started_lock = threading.Lock()
         self._finished_lock = threading.Lock()
         self._bucket_lock = threading.Lock()
+        self._waiting_lock = threading.Lock()
+        self._start_second_lock = threading.Lock()
+
+    def pause(self):
+        rate = int()
+        if self._bucket_second - self._start_second >= self._ramp:
+            rate = self._rate
+        else:
+            rate = round(self._rate * (
+                (self._bucket_second - self._start_second) / self._ramp))
+
+        self._bucket_lock.acquire()
+        self._bucket_entries += 1
+        if (self._bucket_entries >= rate):
+            self._bucket_second += 1
+            self._bucket_entries = 0
+        self._bucket_lock.release()
+
+        now = int(round(time.time()))
+        # Have we fallen behind?
+        if scheduler_state._bucket_second < now:
+            scheduler_state._bucket_second = now + SCHEDULE_BACKOFF
+        time.sleep(scheduler_state._bucket_second - now +\
+                       ((float(random.randint(0, 1000)))/1000 ))
+
 
 def logger(q, log_file):
     outfile = open(log_file, 'a', 1, newline='')
@@ -108,7 +129,7 @@ args = argparser.parse_args()
 test = __import__(args.test)
 conf = __import__(args.conf)
 
-scheduler_state = SchedulerState(test.statuses)
+scheduler_state = SchedulerState(conf.rate, conf.ramp, test.statuses)
 
 # prepare phase, have the params
 params = dict()
@@ -123,6 +144,7 @@ if conf.stack_size is not None:
 for i in (range(1, conf.workers+1)):
     w = threading.Thread(target=worker, args=(test.Test(conf.hosts,
                                                         params), q, i,
+                                              conf.workers,
                                               scheduler_state, conf.rate,
                                               conf.under_threshold,
                                               conf.over_threshold),
