3590c4ea2780f2b035bee1b45411b9c36f02a84f
Mark Travis <mtravis@ripple.com>
2015-02-18 20:53:29 -0800

    Can call admin remotely.

--- benchmark/scripts/fund.py
+++ benchmark/scripts/fund.py
@@ -20,23 +20,22 @@
 import ripplepy
 import argparse
 import json
+import sys
 
 FUNDER = 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh'
 SECRET = 'masterpassphrase'
 DROPS = 1000000
-SERVER = 'http://localhost:51234/'
 
 cfg = {
     'currency': 'BUX',
-    'accounts': 5,
-    'server': 'http://localhost:51234/',
-    'amount_gateway_xrp': str(100 * DROPS),
-    'amount_xrp': str(500 * DROPS),
+    'accounts': 5000,
+    'amount_gateway_xrp': str(100000 * DROPS),
+    'amount_xrp': str(10000000 * DROPS),
     'trust_currency': '1000000000000',
     'amount_currency': '1000000000'
 }
 
-def fund(sender, recipient, amount, secret):
+def fund(server, sender, recipient, amount, secret):
     tx_json = {
         'Flags': 2147483648,
         'TransactionType': 'Payment',
@@ -45,11 +44,11 @@ def fund(sender, recipient, amount, secret):
         'Destination': recipient,
         'Fee': '1000000',
     }
-    tx_json['Sequence'] = ripplepy.Cmd(SERVER).account_info(sender)['result']['account_data']['Sequence']
+    tx_json['Sequence'] = ripplepy.Cmd(server).account_info(sender)['result']['account_data']['Sequence']
     transaction = {'tx_json': tx_json, 'secret': secret}
-    print(ripplepy.Cmd(SERVER).submit(ripplepy.ripple_serdes.ripple_json(json.dumps(transaction).encode())))
+    print(ripplepy.Cmd(server).submit(ripplepy.ripple_serdes.ripple_json(json.dumps(transaction).encode())))
 
-def trust(limit, gateway, sender, secret):
+def trust(server, limit, gateway, sender, secret):
     limitAmount = {
         'currency': cfg['currency'],
         'value': limit,
@@ -62,37 +61,38 @@ def trust(limit, gateway, sender, secret):
         'Fee': '1000000'
     }
     tx_json['LimitAmount'] = limitAmount
-    tx_json['Sequence'] = ripplepy.Cmd(SERVER).account_info(sender)['result']['account_data']['Sequence']
+    tx_json['Sequence'] = ripplepy.Cmd(server).account_info(sender)['result']['account_data']['Sequence']
     transaction = {'tx_json': tx_json, 'secret': secret}
-    print(ripplepy.Cmd(SERVER).submit(ripplepy.ripple_serdes.ripple_json(json.dumps(transaction).encode())))
+    print(ripplepy.Cmd(server).submit(ripplepy.ripple_serdes.ripple_json(json.dumps(transaction).encode())))
 
 # main
 argparser = argparse.ArgumentParser()
 argparser.add_argument('-o', '--output-file', type=str, required=True)
+argparser.add_argument('-s', '--server', type=str, required=True)
 args = argparser.parse_args()
 
 outfile = open(args.output_file, 'w')
 
 res = {'cfg': cfg}
-res['gateway'] = ripplepy.Cmd(SERVER).wallet_propose()['result']
+res['gateway'] = ripplepy.Cmd(args.server).wallet_propose()['result']
 gateway = res['gateway']['account_id']
 gateway_seed = res['gateway']['master_seed']
-fund(FUNDER, gateway, cfg['amount_gateway_xrp'], SECRET)
+fund(args.server, FUNDER, gateway, cfg['amount_gateway_xrp'], SECRET)
 
 gateway = res['gateway']['account_id']
 
 res['accounts'] = list()
 for i in range(1, cfg['accounts']+1):
-    account = ripplepy.Cmd(SERVER).wallet_propose()['result']
-    fund(FUNDER, account['account_id'], cfg['amount_xrp'], SECRET)
+    account = ripplepy.Cmd(args.server).wallet_propose()['result']
+    fund(args.server, FUNDER, account['account_id'], cfg['amount_xrp'], SECRET)
     amount = {
         'currency': cfg['currency'],
         'value': cfg['amount_currency'],
         'issuer': gateway
     }
-    trust(cfg['trust_currency'], gateway, account['account_id'],
+    trust(args.server, cfg['trust_currency'], gateway, account['account_id'],
          account['master_seed'])
-    fund(gateway, account['account_id'], amount, gateway_seed)
+    fund(args.server, gateway, account['account_id'], amount, gateway_seed)
     res['accounts'].append(account)
 
 json.dump(res, outfile)
